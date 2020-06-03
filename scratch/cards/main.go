b3e26aa6b8cf487c02f36f41938b5ca72ef5c087
Mike Williams <mumakin@gmail.com>
2020-06-03 14:45:06 -0600

    add a slice of cards, print in range loop

--- scratch/cards/main.go
+++ scratch/cards/main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
-	fmt.Println(card)
+	cards := []string{newCard(), "Ace of Diamonds"}
+	cards = append(cards, "Siz of Spades")
+
+	for index, card := range cards {
+		fmt.Println(index, card)
+	}
 }
 
 func newCard() string {
