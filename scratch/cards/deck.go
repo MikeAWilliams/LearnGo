391b8c0311ac7b5d4bb48c91dd58dd8b1b8376e8
Mike Williams <mumakin@gmail.com>
2020-06-03 15:19:34 -0600

    newDeck works

--- scratch/cards/deck.go
+++ scratch/cards/deck.go
@@ -9,3 +9,17 @@ func (d deck) print() {
 		fmt.Println(index, card)
 	}
 }
+
+func newDeck() deck {
+	suits := []string{"Spades", "Clubs", "Hearts", "Diamonds"}
+	values := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	result := deck{}
+
+	for _, suit := range suits {
+		for _, value := range values {
+			result = append(result, value+" of "+suit)
+		}
+	}
+	return result
+}
--- scratch/cards/main.go
+++ scratch/cards/main.go
@@ -1,8 +1,7 @@
 package main
 
 func main() {
-	cards := deck{newCard(), "Ace of Diamonds"}
-	cards = append(cards, "Siz of Spades")
+	cards := newDeck()
 	cards.print()
 }
 
