8779fb12b4fbf36dc84db997436b619625b10f03
Mike Williams <mumakin@gmail.com>
2020-06-03 22:09:44 -0600

    test better

--- scratch/cards/deck_test.go
+++ scratch/cards/deck_test.go
@@ -2,9 +2,25 @@ package main
 
 import "testing"
 
+func uniqueCards(input deck) deck {
+	foundAlready := map[string]bool{}
+	result := deck{}
+	for _, card := range input {
+		if !foundAlready[card] {
+			result = append(result, card)
+		}
+	}
+	return result
+}
 func TestNewDeck(t *testing.T) {
 	testObject := newDeck()
 	if 52 != len(testObject) {
-		t.Errorf("Expected length of 52 but got %d", len(testObject))
+		t.Errorf("Expected len(testObject) of 52 but got %d", len(testObject))
 	}
+
+	uniqueCardsInTestObject := uniqueCards(testObject)
+	if 52 != len(uniqueCardsInTestObject) {
+		t.Errorf("Expected len(uniqueCardsInTestObject) of 52 but got %d", len(uniqueCardsInTestObject))
+	}
+
 }
