0c0001f481048e13677001fb834281c9fc749fc8
Michael Williams <mwilliams@alteryx.com>
2020-06-26 15:51:50 -0600

    fully tested addItem

--- todo/busineslogic/businesslogic.go
+++ todo/busineslogic/businesslogic.go
@@ -14,12 +14,32 @@ func (item TodoItem) String() string {
 
 type Database interface {
 	GetItem(title string) (TodoItem, error)
+	HasItem(title string) (bool, error)
 	GetAllItems() ([]TodoItem, error)
-	AddItem(item TodoItem) (bool, TodoItem)
+	AddItem(item TodoItem) error
 }
 
-func AddItem(title string, description string, db Database) (bool, TodoItem) {
-	return db.AddItem(TodoItem{title, description, false})
+func AddItem(title string, description string, db Database) (bool, TodoItem, error) {
+	hasItem, hasErr := db.HasItem(title)
+	if nil != hasErr {
+		return false, TodoItem{}, hasErr
+	}
+
+	if hasItem {
+		existingItem, getErr := db.GetItem(title)
+		if nil != getErr {
+			return false, TodoItem{}, getErr
+		}
+		return false, existingItem, nil
+	}
+
+	toAdd := TodoItem{title, description, false}
+	addErr := db.AddItem(toAdd)
+	if nil != addErr {
+		return false, TodoItem{}, addErr
+	}
+
+	return true, toAdd, nil
 }
 
 func PrintTodoItem(item TodoItem) {
--- todo/testbusinessLogic/businessLogic_test.go
+++ todo/testbusinessLogic/businessLogic_test.go
@@ -9,32 +9,110 @@ import (
 	"github.com/MikeAWilliams/LearnGo/tree/master/todo/busineslogic"
 )
 
-type dbSpy struct {
+type dbDouble struct {
 	lastAdded busineslogic.TodoItem
 }
 
-func (db *dbSpy) GetItem(title string) (busineslogic.TodoItem, error) {
+func (db *dbDouble) GetItem(title string) (busineslogic.TodoItem, error) {
 	if db.lastAdded.Title == title {
 		return db.lastAdded, nil
 	}
 	return busineslogic.TodoItem{}, errors.New("The item is not in the db")
 }
-func (db *dbSpy) GetAllItems() ([]busineslogic.TodoItem, error) {
+
+func (db *dbDouble) HasItem(title string) (bool, error) {
+	if db.lastAdded.Title == title {
+		return true, nil
+	}
+	return false, nil
+}
+
+func (db *dbDouble) GetAllItems() ([]busineslogic.TodoItem, error) {
 	return []busineslogic.TodoItem{}, errors.New("Not implemented")
 }
 
-func (db *dbSpy) AddItem(item busineslogic.TodoItem) (bool, busineslogic.TodoItem) {
+func (db *dbDouble) AddItem(item busineslogic.TodoItem) error {
 	if db.lastAdded.Title == item.Title {
-		return false, db.lastAdded
+		return errors.New("the item is there")
 	}
 	db.lastAdded = item
-	return true, item
+	return nil
 }
 
-func TestMySpy_AddItem(t *testing.T) {
-	spy := dbSpy{}
-	toAdd := busineslogic.TodoItem{"title", "description", false}
-	addWorked, added := spy.AddItem(toAdd)
-	require.True(t, addWorked)
-	require.Equal(t, toAdd, added)
+func TestBusinessLogic_AddItem_ItemNotInDB(t *testing.T) {
+	db := dbDouble{}
+
+	wasAdded, item, err := busineslogic.AddItem("Laundry", "Clean laundry", &db)
+	require.Equal(t, nil, err)
+	require.True(t, wasAdded)
+	require.Equal(t, "Laundry", item.Title)
+	require.Equal(t, "Clean laundry", item.Description)
+	require.False(t, item.Complete)
+
+	require.Equal(t, item, db.lastAdded)
+}
+
+func TestBusinessLogic_AddItem_ItemInDB(t *testing.T) {
+	toAdd := busineslogic.TodoItem{"Laundry", "Clean laundry", true}
+	db := dbDouble{toAdd}
+
+	wasAdded, item, err := busineslogic.AddItem(toAdd.Title, toAdd.Description, &db)
+	require.Equal(t, nil, err)
+	require.False(t, wasAdded)
+	require.Equal(t, toAdd, item)
+}
+
+type dbErr struct {
+	methodCalls int
+	hasItemErr  error
+	getItemErr  error
+	addItemErr  error
+	hasItem     bool
+}
+
+func (db *dbErr) GetItem(title string) (busineslogic.TodoItem, error) {
+	db.methodCalls++
+	return busineslogic.TodoItem{}, db.getItemErr
+}
+
+func (db *dbErr) HasItem(title string) (bool, error) {
+	db.methodCalls++
+	return db.hasItem, db.hasItemErr
+}
+
+func (db *dbErr) GetAllItems() ([]busineslogic.TodoItem, error) {
+	db.methodCalls++
+	return []busineslogic.TodoItem{}, errors.New("Not implemented")
+}
+
+func (db *dbErr) AddItem(item busineslogic.TodoItem) error {
+	db.methodCalls++
+	return db.addItemErr
+}
+
+func TestBusinessLogic_AddItem_HasItemIsError(t *testing.T) {
+	db := dbErr{0, errors.New("Error"), nil, nil, false}
+
+	wasAdded, _, err := busineslogic.AddItem("Laundry", "Clean laundry", &db)
+	require.Equal(t, db.hasItemErr, err)
+	require.Equal(t, 1, db.methodCalls)
+	require.False(t, wasAdded)
+}
+
+func TestBusinessLogic_AddItem_GetItemIsError(t *testing.T) {
+	db := dbErr{0, nil, errors.New("Error"), nil, true}
+
+	wasAdded, _, err := busineslogic.AddItem("Laundry", "Clean laundry", &db)
+	require.Equal(t, db.getItemErr, err)
+	require.Equal(t, 2, db.methodCalls)
+	require.False(t, wasAdded)
+}
+
+func TestBusinessLogic_AddItem_AddItemIsError(t *testing.T) {
+	db := dbErr{0, nil, nil, errors.New("Error"), false}
+
+	wasAdded, _, err := busineslogic.AddItem("Laundry", "Clean laundry", &db)
+	require.Equal(t, db.addItemErr, err)
+	require.Equal(t, 2, db.methodCalls)
+	require.False(t, wasAdded)
 }
