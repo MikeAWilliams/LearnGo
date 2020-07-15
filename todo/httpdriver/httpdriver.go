ef3309f7818baad6d07762eafdc6b7b0a18cf302
Mike Williams <mumakin@gmail.com>
2020-07-14 22:46:32 -0600

    support delete

--- todo/httpdriver/httpdriver.go
+++ todo/httpdriver/httpdriver.go
@@ -18,10 +18,6 @@ func errorWasHandled(w http.ResponseWriter, err error) bool {
 	return false
 }
 
-func getSlashHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Gorilla!\n"))
-}
-
 func getGetItemsHandler(db busineslogic.Database) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		items, err := busineslogic.GetAllItems(db)
@@ -108,14 +104,28 @@ func getPutSpecificItemHandler(db busineslogic.Database) func(http.ResponseWrite
 	}
 }
 
+func getDeleteSpecificItemHandler(db busineslogic.Database) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		item, err := busineslogic.DeleteItem(title, db)
+		if errorWasHandled(w, err) {
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(item)
+	}
+}
+
 func Start(db busineslogic.Database) {
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
-	r.HandleFunc("/", getSlashHandler).Methods("GET")
 	r.HandleFunc("/api/v1/items", getGetItemsHandler(db)).Methods("GET")
 	r.HandleFunc("/api/v1/items/{title}", getGetSpecificItemHandler(db)).Methods("GET")
 	r.HandleFunc("/api/v1/items/{title}", getPostSpecificItemHandler(db)).Methods("POST")
 	r.HandleFunc("/api/v1/items/{title}", getPutSpecificItemHandler(db)).Methods("PUT")
+	r.HandleFunc("/api/v1/items/{title}", getDeleteSpecificItemHandler(db)).Methods("DELETE")
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":8000", r))
