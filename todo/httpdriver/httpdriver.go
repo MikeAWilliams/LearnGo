e5875a1f09a20da6e334bae1745b488abbf72944
Mike Williams <mumakin@gmail.com>
2020-07-14 22:34:53 -0600

    add post

--- todo/httpdriver/httpdriver.go
+++ todo/httpdriver/httpdriver.go
@@ -48,12 +48,41 @@ func getGetSpecificItemHandler(db busineslogic.Database) func(http.ResponseWrite
 	}
 }
 
+type postItemBody struct {
+	Description string
+}
+
+func getPostSpecificItemHandler(db busineslogic.Database) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		var body postItemBody
+		body.Description = "not set"
+		bodyError := json.NewDecoder(r.Body).Decode(&body)
+		if errorWasHandled(w, bodyError) {
+			return
+		}
+
+		added, item, err := busineslogic.AddItem(title, body.Description, db)
+		if errorWasHandled(w, err) {
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		if !added {
+			w.WriteHeader(http.StatusNonAuthoritativeInfo)
+		}
+		json.NewEncoder(w).Encode(item)
+	}
+}
+
 func Start(db busineslogic.Database) {
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
 	r.HandleFunc("/", getSlashHandler).Methods("GET")
 	r.HandleFunc("/api/v1/items", getGetItemsHandler(db)).Methods("GET")
 	r.HandleFunc("/api/v1/items/{title}", getGetSpecificItemHandler(db)).Methods("GET")
+	r.HandleFunc("/api/v1/items/{title}", getPostSpecificItemHandler(db)).Methods("POST")
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":8000", r))
