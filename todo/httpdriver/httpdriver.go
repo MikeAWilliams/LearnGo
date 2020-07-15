f2d5839342c0273158f93e22435f2b796f0614e9
Mike Williams <mumakin@gmail.com>
2020-07-14 21:30:01 -0600

    pattern matching in routing works

--- todo/httpdriver/httpdriver.go
+++ todo/httpdriver/httpdriver.go
@@ -27,11 +27,26 @@ func getGetItemsHandler(db busineslogic.Database) func(http.ResponseWriter, *htt
 	}
 }
 
+func getGetSpecificItemHandler(db busineslogic.Database) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		item, err := busineslogic.GetItem(title, db)
+		if nil != err {
+			w.Write([]byte(err.Error()))
+		}
+
+		result := item.String() + "\n"
+		w.Write([]byte(result))
+	}
+}
+
 func Start(db busineslogic.Database) {
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
 	r.HandleFunc("/", getSlashHandler).Methods("GET")
 	r.HandleFunc("/api/v1/items", getGetItemsHandler(db)).Methods("GET")
+	r.HandleFunc("/api/v1/items/{title}", getGetSpecificItemHandler(db)).Methods("GET")
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":8000", r))
