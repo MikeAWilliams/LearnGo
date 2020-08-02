33833d4b2b8179da87d57b19b116856467812618
Mike Williams <mumakin@gmail.com>
2020-08-01 22:16:30 -0600

    add apis and manual test http file

--- authExamples/auth0/main.go
+++ authExamples/auth0/main.go
@@ -14,6 +14,15 @@ func main() {
 	// We will setup our server so we can serve static assest like images, css from the /static/{file} route
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
+	// apis
+	r.Handle("/status", NotImplemented).Methods("GET")
+	r.Handle("/products", NotImplemented).Methods("GET")
+	r.Handle("/products/{slug}/feedback", NotImplemented).Methods("POST")
+
 	// Our application will run on port 8080. Here we declare the port and pass in our router.
 	http.ListenAndServe(":8080", r)
 }
+
+var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Not Implemented"))
+})
