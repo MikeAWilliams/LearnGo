1ada41a0977663578e1b9bbaca95681968e0086e
Mike Williams <mumakin@gmail.com>
2020-08-01 22:08:44 -0600

    basic webserver running for auth example

---
+++ authExamples/auth0/main.go
@@ -0,0 +1,19 @@
+// following the example from https://auth0.com/blog/authentication-in-golang/
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.Handle("/", http.FileServer(http.Dir("./views/")))
+	// We will setup our server so we can serve static assest like images, css from the /static/{file} route
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	// Our application will run on port 8080. Here we declare the port and pass in our router.
+	http.ListenAndServe(":8080", r)
+}
