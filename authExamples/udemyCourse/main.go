2b8383618bca9a0cd61eb0191ae7da7763d1dcca
Mike Williams <mumakin@gmail.com>
2020-09-15 21:20:38 -0600

    some types

--- authExamples/udemyCourse/main.go
+++ authExamples/udemyCourse/main.go
@@ -8,6 +8,20 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type User struct {
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type JWT struct {
+	Token string `json:"token"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/signup", signup).Methods("POST")
