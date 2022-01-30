8019c4f863b551f56c550dd3a48811413cde2ab3
Mike Williams <mumakin@gmail.com>
2022-01-30 16:59:18 -0700

    some simple test cases work

--- graphics/lineGame/game/board_test.go
+++ graphics/lineGame/game/board_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNearestPointBoardIsSingleLineUp(t *testing.T) {
+func TestNearestPoint_BoardIsSingleLineUp_TestPointIsOnSegment(t *testing.T) {
 	p1 := game.Point{X: float64(0), Y: float64(0)}
 	p2 := game.Point{X: float64(0), Y: float64(1)}
 	testObject := game.NewBoard([]game.Segment{{P1: p1, P2: p2}})
@@ -33,5 +33,96 @@ func TestNearestPointBoardIsSingleLineUp(t *testing.T) {
 	require.Equal(t, 1.0, rp.Y)
 	found, rp = testObject.NearestPoint(game.Point{X: float64(0), Y: float64(0.5)})
 	require.True(t, found)
+	require.Equal(t, 0.0, rp.X)
 	require.Equal(t, 0.5, rp.Y)
+}
+
+func TestNearestPoint_BoardIsSingleLineUp_TestPointIsRightOfSegment(t *testing.T) {
+	p1 := game.Point{X: float64(0), Y: float64(0)}
+	p2 := game.Point{X: float64(0), Y: float64(1)}
+	testObject := game.NewBoard([]game.Segment{{P1: p1, P2: p2}})
+
+	// above
+	found, _ := testObject.NearestPoint(game.Point{X: float64(1), Y: float64(3)})
+	require.False(t, found)
+	found, _ = testObject.NearestPoint(game.Point{X: float64(1), Y: float64(1.1)})
+	require.False(t, found)
+
+	// below
+	found, _ = testObject.NearestPoint(game.Point{X: float64(1), Y: float64(-0.1)})
+	require.False(t, found)
+
+	// inside
+	found, rp := testObject.NearestPoint(game.Point{X: float64(1), Y: float64(0.0)})
+	require.True(t, found)
+	require.Equal(t, 0.0, rp.X)
+	require.Equal(t, 0.0, rp.Y)
+	found, rp = testObject.NearestPoint(game.Point{X: float64(1), Y: float64(1.0)})
+	require.True(t, found)
+	require.Equal(t, 0.0, rp.X)
+	require.Equal(t, 1.0, rp.Y)
+	found, rp = testObject.NearestPoint(game.Point{X: float64(1), Y: float64(0.5)})
+	require.True(t, found)
+	require.Equal(t, 0.0, rp.X)
+	require.Equal(t, 0.5, rp.Y)
+}
+
+func TestNearestPoint_BoardIsSingleLineUp_TestPointIsLeftOfSegment(t *testing.T) {
+	p1 := game.Point{X: float64(0), Y: float64(0)}
+	p2 := game.Point{X: float64(0), Y: float64(1)}
+	testObject := game.NewBoard([]game.Segment{{P1: p1, P2: p2}})
+
+	// above
+	found, _ := testObject.NearestPoint(game.Point{X: float64(-1), Y: float64(3)})
+	require.False(t, found)
+	found, _ = testObject.NearestPoint(game.Point{X: float64(-1), Y: float64(1.1)})
+	require.False(t, found)
+
+	// below
+	found, _ = testObject.NearestPoint(game.Point{X: float64(-1), Y: float64(-0.1)})
+	require.False(t, found)
+
+	// inside
+	found, rp := testObject.NearestPoint(game.Point{X: float64(-1), Y: float64(0.0)})
+	require.True(t, found)
+	require.Equal(t, 0.0, rp.X)
+	require.Equal(t, 0.0, rp.Y)
+	found, rp = testObject.NearestPoint(game.Point{X: float64(-1), Y: float64(1.0)})
+	require.True(t, found)
+	require.Equal(t, 0.0, rp.X)
+	require.Equal(t, 1.0, rp.Y)
+	found, rp = testObject.NearestPoint(game.Point{X: float64(-1), Y: float64(0.5)})
+	require.True(t, found)
+	require.Equal(t, 0.0, rp.X)
+	require.Equal(t, 0.5, rp.Y)
+}
+
+func TestNearestPoint_BoardIsSingleLineRight(t *testing.T) {
+	p1 := game.Point{X: float64(0), Y: float64(0)}
+	p2 := game.Point{X: float64(1), Y: float64(0)}
+	testObject := game.NewBoard([]game.Segment{{P1: p1, P2: p2}})
+
+	// above
+	found, _ := testObject.NearestPoint(game.Point{X: float64(3), Y: float64(0)})
+	require.False(t, found)
+	found, _ = testObject.NearestPoint(game.Point{X: float64(1.1), Y: float64(0)})
+	require.False(t, found)
+
+	// below
+	found, _ = testObject.NearestPoint(game.Point{X: float64(-0.1), Y: float64(0)})
+	require.False(t, found)
+
+	// inside
+	found, rp := testObject.NearestPoint(game.Point{X: float64(0.0), Y: float64(0.0)})
+	require.True(t, found)
+	require.Equal(t, 0.0, rp.X)
+	require.Equal(t, 0.0, rp.Y)
+	found, rp = testObject.NearestPoint(game.Point{X: float64(1.0), Y: float64(0)})
+	require.True(t, found)
+	require.Equal(t, 1.0, rp.X)
+	require.Equal(t, 0.0, rp.Y)
+	found, rp = testObject.NearestPoint(game.Point{X: float64(0.5), Y: float64(0)})
+	require.True(t, found)
+	require.Equal(t, 0.5, rp.X)
+	require.Equal(t, 0.0, rp.Y)
 }
