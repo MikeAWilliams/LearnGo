af076ddd12f35eb6ef75eecd44bdca577eae435a
Mike Williams <mumakin@gmail.com>
2022-01-30 17:08:54 -0700

    square tests

--- graphics/lineGame/game/board_test.go
+++ graphics/lineGame/game/board_test.go
@@ -126,3 +126,60 @@ func TestNearestPoint_BoardIsSingleLineRight(t *testing.T) {
 	require.Equal(t, 0.5, rp.X)
 	require.Equal(t, 0.0, rp.Y)
 }
+
+func TestNearestPoint_BoardIsSquare(t *testing.T) {
+	p1 := game.Point{X: float64(0), Y: float64(0)}
+	p2 := game.Point{X: float64(1), Y: float64(0)}
+	p3 := game.Point{X: float64(1), Y: float64(1)}
+	p4 := game.Point{X: float64(0), Y: float64(1)}
+	p5 := game.Point{X: float64(0), Y: float64(0)}
+	testObject := game.NewBoard([]game.Segment{{P1: p1, P2: p2}, {P1: p2, P2: p3}, {P1: p3, P2: p4}, {P1: p4, P2: p5}})
+
+	//right side
+	testPoint := game.Point{X: float64(1.1), Y: float64(0.5)}
+	found, result := testObject.NearestPoint(testPoint)
+	require.True(t, found)
+	require.Equal(t, 1.0, result.X)
+	require.Equal(t, 0.5, result.Y)
+	testPoint.X = float64(0.9)
+	found, result = testObject.NearestPoint(testPoint)
+	require.True(t, found)
+	require.Equal(t, 1.0, result.X)
+	require.Equal(t, 0.5, result.Y)
+
+	//left side
+	testPoint = game.Point{X: float64(0.1), Y: float64(0.5)}
+	found, result = testObject.NearestPoint(testPoint)
+	require.True(t, found)
+	require.Equal(t, 0.0, result.X)
+	require.Equal(t, 0.5, result.Y)
+	testPoint.X = float64(-0.1)
+	found, result = testObject.NearestPoint(testPoint)
+	require.True(t, found)
+	require.Equal(t, 0.0, result.X)
+	require.Equal(t, 0.5, result.Y)
+
+	//top side
+	testPoint = game.Point{X: float64(0.5), Y: float64(1.1)}
+	found, result = testObject.NearestPoint(testPoint)
+	require.True(t, found)
+	require.Equal(t, 0.5, result.X)
+	require.Equal(t, 1.0, result.Y)
+	testPoint.Y = float64(0.9)
+	found, result = testObject.NearestPoint(testPoint)
+	require.True(t, found)
+	require.Equal(t, 0.5, result.X)
+	require.Equal(t, 1.0, result.Y)
+
+	//bottom side
+	testPoint = game.Point{X: float64(0.5), Y: float64(0.1)}
+	found, result = testObject.NearestPoint(testPoint)
+	require.True(t, found)
+	require.Equal(t, 0.5, result.X)
+	require.Equal(t, 0.0, result.Y)
+	testPoint.Y = float64(-0.1)
+	found, result = testObject.NearestPoint(testPoint)
+	require.True(t, found)
+	require.Equal(t, 0.5, result.X)
+	require.Equal(t, 0.0, result.Y)
+}
